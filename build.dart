27bf461dd2463046bf2de2fde41fa0b3c88ccc5a
Vinícius Antonioli <viniciusantonioli.dev@gmail.com>
2020-11-17 20:35:13 -0300

    v0.0.4-dev

--- build.dart
+++ build.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 void main() {
-  Process.run('dart2native', ['bin/main.dart', '-o', 'build/gitloop']).then((ProcessResult results) {
+  Process.run('dart2native', ['bin/main.dart', '-o', 'build/gitloop'])
+      .then((ProcessResult results) {
     print('Compilado com sucesso!');
   });
 }
